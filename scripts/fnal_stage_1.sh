405af2f76b7b4e76ce0bebde1fa08b3b16012dc3
Eric Vaandering <ericvaandering@gmail.com>
2021-10-12 15:42:15 -0500

    Use the right location

--- scripts/fnal_stage_1.sh
+++ scripts/fnal_stage_1.sh
@@ -3,7 +3,7 @@
 
 cp ~/CTAEvaluation/replacements/00-cta-tape.rules ~/CTA/continuousintegration/buildtree_runner/00-cta-tape.rules
 # Problems with Ceph certificate on October 12, 2021
-cp ~/CTAEvaluation/replacements/bootstrapCTA.sh ~/CTA/continuousintegration/buildtree_runner/vmBootstrap
+cp ~/CTAEvaluation/replacements/FNAL/bootstrapCTA.sh ~/CTA/continuousintegration/buildtree_runner/vmBootstrap
 cd ~/CTA/continuousintegration/buildtree_runner/vmBootstrap
 ./bootstrapCTA.sh
 
