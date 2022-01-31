c0e2ccfc184c0cbb25f00afa5fbe04387bf12edc
Eric Vaandering <ewv@fnal.gov>
2022-01-31 23:48:03 +0100

    Files after simple test

---
+++ scripts/prepare_cpio_test_cern.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+set -x
+
+export NAMESPACE=cta
+cd ~/CTA/continuousintegration/orchestration || exit
+#./delete_instance.sh -n cta; sleep 30; sudo bash -xv ./create_instance.sh -n cta -b ~ -B CTA-build -D -O -d database.yaml
+cd tests/ || exit
+#sleep 15
+./prepare_tests.sh -n ${NAMESPACE}
+kubectl -n ${NAMESPACE} cp simple_client_ar.sh client:/root/simple_client_ar.sh
+kubectl -n ${NAMESPACE} cp client_helper.sh client:/root/client_helper.sh
+kubectl -n ${NAMESPACE} cp ~/CTAEvaluation/scripts/cpio_simple_ar.sh client:/root/cpio_simple_ar.sh
+kubectl -n ${NAMESPACE} cp ~/CTAEvaluation/scripts/cpio_big_ar.sh client:/root/cpio_big_ar.sh
+
+kubectl exec -it -n cta client -- bash
