b7d89414efb3297c779a697770dd3b3647768fa0
Eric Vaandering <ericvaandering@gmail.com>
2022-02-01 16:38:55 -0600

    My build script

---
+++ scripts/build_csv_migrate.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+# mkdir ~/ewvCTA
+# cd ~/ewvCTA
+#  git clone https://gitlab.cern.ch/ewv/CTA.git
+# cd ~/ewvCTA/CTA; git checkout --track origin/hack_cpio
+
+cd ~/ewvCTA/CTA; git pull
+cp ~/ewvCTA/CTA/migration/gRPC/CMakeLists.txt    ~/CTA/migration/gRPC/CMakeLists.txt
+cp ~/ewvCTA/CTA/migration/gRPC/EosImportFilesCSV.cpp    ~/CTA/migration/gRPC/EosImportFilesCSV.cpp
+cp ~/ewvCTA/CTA/migration/gRPC/EosImportFiles.cpp    ~/CTA/migration/gRPC/EosImportFiles.cpp
+cp ~/ewvCTA/CTA/migration/gRPC/EosImportFiles.hpp    ~/CTA/migration/gRPC/EosImportFiles.hpp
+
+cd ~/CTA-build && cmake ../CTA; make -j 4
+
+
