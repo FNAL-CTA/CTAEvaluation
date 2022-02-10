962aa0b6cc35729d3dd8b15adb2e5ec6e72d431c
Eric Vaandering <ericvaandering@gmail.com>
2022-02-10 10:52:09 -0600

    Build C++ code too

--- scripts/build_csv_migrate.sh
+++ scripts/build_csv_migrate.sh
@@ -10,7 +10,7 @@ cp ~/ewvCTA/CTA/migration/gRPC/CMakeLists.txt    ~/CTA/migration/gRPC/CMakeLists
 cp ~/ewvCTA/CTA/migration/gRPC/EosImportFilesCSV.cpp    ~/CTA/migration/gRPC/EosImportFilesCSV.cpp
 cp ~/ewvCTA/CTA/migration/gRPC/EosImportFiles.cpp    ~/CTA/migration/gRPC/EosImportFiles.cpp
 cp ~/ewvCTA/CTA/migration/gRPC/EosImportFiles.hpp    ~/CTA/migration/gRPC/EosImportFiles.hpp
-
+cp ~/ewvCTA/CTA/migration/gRPC/GrpcClient.cpp ~/CTA/migration/gRPC/GrpcClient.cpp
 cd ~/CTA-build && cmake ../CTA; make -j 4
 
 
