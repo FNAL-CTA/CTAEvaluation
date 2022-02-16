648749a658c24fd3e9ed3b2579b90cb389599174
Eric Vaandering <ericvaandering@gmail.com>
2022-02-16 11:36:20 -0600

    Add the container and a script to start to migrate files

--- scripts/cern_stage_1.sh
+++ scripts/cern_stage_1.sh
@@ -10,6 +10,9 @@ sudo cp ~/CTAEvaluation/replacements/CERN/98-noipv6.conf /etc/sysctl.d/
 
 sudo sysctl --system
 sudo yum install -y git tmux nano
+#sudo yum install -y git tmux nano pip3
+#sudo pip3 install protobuf
+#sudo pip3 install sqlalchemy
 
 git clone https://gitlab.cern.ch/cta/CTA.git
 cd CTA/continuousintegration/buildtree_runner/vmBootstrap
@@ -17,4 +20,4 @@ cd CTA/continuousintegration/buildtree_runner/vmBootstrap
 
 sudo -u cta bash -c 'cd ~ ; git clone https://github.com/ericvaandering/CTAEvaluation.git'
 
-echo "su - cta then ~/CTAEvaluation/scripts/cern_stage_3.sh"
+echo "su - cta then ~/CTAEvaluation/scripts/cern_stage_2.sh"
