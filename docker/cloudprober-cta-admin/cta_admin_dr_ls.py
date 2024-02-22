b24f34ddcbe9d73a68a56318882be2177dd92fd3
Eric Vaandering <ewv@fnal.gov>
2024-02-22 16:50:22 -0600

    Add drive ls probe

--- docker/cloudprober-cta-admin/cta_admin_dr_ls.py
+++ docker/cloudprober-cta-admin/cta_admin_dr_ls.py
@@ -1,51 +1,50 @@
 #! /usr/bin/env python3
 
-import subprocess
 import json
+import subprocess
 
 
 def produce_prom_metric(metric_name, value, drive_list, labels):
-  #loop over labels to get key,value pairs
-  #remove drives that we do not care about with the if not
-  #import pdb; pdb.set_trace()
-  #print(len(labels))  
-  label_list=[]
-  for label in labels:
-    #print(label)
-    label_list+=[label+'="'+drive_list[label]+'"']
-    #print(label_list)
-  if not 'driveName="DRIVE0"' in label_list:
-          print(metric_name, end='')
-          print('{',', '.join(label_list),end='')#,sep = ", "
-          print('}', end='')
-          print(f' {value}')
+    # loop over labels to get key,value pairs
+    # remove drives that we do not care about with the if not
+    # import pdb; pdb.set_trace()
+    # print(len(labels))
+    label_list = []
+    for label in labels:
+        # print(label)
+        label_list += [label + '="' + drive_list[label] + '"']
+        # print(label_list)
+    if not 'driveName="DRIVE0"' in label_list:
+        print(metric_name, end='')
+        print('{', ', '.join(label_list), end='')  # ,sep = ", "
+        print('}', end='')
+        print(f' {value}')
 
 
-drLS_output = subprocess.check_output(["XrdSecPROTOCOL=sss XrdSecSSSKT=/etc/ctafrontend_client_sss.keytab cta-admin --json dr ls"], shell=True)
+drLS_output = subprocess.check_output(["cta-admin --json dr ls"], shell=True)
 
 driveLS_dict = json.loads(drLS_output)
 
-#print(driveLS_dict)
+# print(driveLS_dict)
 bytes_per_session = 0.0
 files_per_session = 0.0
 
 for drive_list in driveLS_dict:
-    #extracting the value of the information from the dr ls cta-admin command
-    #print(drive)
+    # extracting the value of the information from the dr ls cta-admin command
+    # print(drive)
     bytes_transferred = int(drive_list['bytesTransferredInSession'])
     files_transferred = int(drive_list['filesTransferredInSession'])
-    session_time= int(drive_list['sessionElapsedTime'])
-    if session_time==0:
-        bytes_per_session=0
-        files_per_session=0
-    elif session_time>0:
-        bytes_per_session=bytes_transferred/session_time
-        files_per_session=filesTran/session_time
-    #formatting to look like what parmethus wants
-    DriveLSLabels=['vo', 'driveName', 'logicalLibrary','sessionId','tapepool'] 
-    produce_prom_metric('bytes_transferred', bytes_transferred, drive_list, labels=DriveLSLabels)
-    produce_prom_metric('files_transferred', files_transferred, drive_list,labels=DriveLSLabels)
-    produce_prom_metric('session_time', session_time, drive_list,labels=DriveLSLabels)
-    produce_prom_metric('bytes_per_session', bytes_per_session, drive_list,labels=DriveLSLabels)
-    produce_prom_metric('files_per_session', files_per_session, drive_list,labels=DriveLSLabels)
-     
+    session_time = int(drive_list['sessionElapsedTime'])
+    if session_time == 0:
+        bytes_per_session = 0
+        files_per_session = 0
+    elif session_time > 0:
+        bytes_per_session = bytes_transferred / session_time
+        files_per_session = files_transferred / session_time
+    # formatting to look like what parmethus wants
+    DriveLSLabels = ['vo', 'driveName', 'logicalLibrary', 'sessionId', 'tapepool']
+    produce_prom_metric('drive_session_bytes', bytes_transferred, drive_list, labels=DriveLSLabels)
+    produce_prom_metric('drive_session_files', files_transferred, drive_list, labels=DriveLSLabels)
+    produce_prom_metric('drive_session_time', session_time, drive_list, labels=DriveLSLabels)
+    produce_prom_metric('drive_session_data_rate', bytes_per_session, drive_list, labels=DriveLSLabels)
+    produce_prom_metric('drive_session_file_rate', files_per_session, drive_list, labels=DriveLSLabels)
