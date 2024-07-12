373a24f2607dda745d03cb198851e1cae6332279
Eric Vaandering <ewv@fnal.gov>
2024-07-12 11:29:55 -0500

    Cleanup frontend image and serve tape types

--- docker/cta-frontend/entrypoint.sh
+++ docker/cta-frontend/entrypoint.sh
@@ -4,17 +4,5 @@
 cp /etc/cta/eos.sss.keytab /etc/cta-nobody/
 chmod 600 /etc/cta-nobody/eos.sss.keytab
 
-# We don't need this long term, for local testing
-cp /etc/cta/forwardable.sss.keytab /etc/cta-nobody/
-chmod 600 /etc/cta-nobody/forwardable.sss.keytab
-
-# Make a file-based OS, initialize it
-mkdir /tmp/os
-cta-objectstore-initialize /tmp/os
-
-# Start it here
-
+# Start the server
 /usr/bin/xrootd -l /var/log/cta/cta-frontend-xrootd.log -c /etc/cta/cta-frontend-xrootd.conf -k fifo -n cta
-
-# Remove this once it's working
-sleep 120
