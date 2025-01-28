7431d8dec6faeb0a646647db13331b561a4aca70
Eric Vaandering <ewv@fnal.gov>
2025-01-28 13:47:55 -0600

    Update builds for 4 containers

--- docker/cta-cron/build_image.sh
+++ docker/cta-cron/build_image.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-podman build --net host . -t imageregistry.fnal.gov/cta/cta-cron:latest
-podman push imageregistry.fnal.gov/cta/cta-cron:latest
+podman build --net host . -t imageregistry.fnal.gov/cta/cta-cron:5.11.2.fnal1
+podman push imageregistry.fnal.gov/cta/cta-cron:5.11.2.fnal1
 
--- docker/cta-frontend/build_image.sh
+++ docker/cta-frontend/build_image.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-podman build --net host . -t imageregistry.fnal.gov/cta/cta-frontend:5.10.10.fnal0
-podman push imageregistry.fnal.gov/cta/cta-frontend:5.10.10.fnal0
+podman build --net host . -t imageregistry.fnal.gov/cta/cta-frontend:5.11.2.fnal1
+podman push imageregistry.fnal.gov/cta/cta-frontend:5.11.2.fnal1
 
