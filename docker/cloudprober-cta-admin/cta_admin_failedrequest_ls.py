d9bac1b7b62452a18e4d25498012b22f148f7610
danijelaconc <dmc24@stmarys-ca.edu>
2025-07-02 11:40:22 -0500

    adding quotations into cta_admin_output

--- docker/cloudprober-cta-admin/cta_admin_failedrequest_ls.py
+++ docker/cloudprober-cta-admin/cta_admin_failedrequest_ls.py
@@ -20,7 +20,7 @@ def produce_prom_metric(metric_name, metric_value, list_input, labels):
     print(f' {metric_value}')
 
 
-cta_admin_output = subprocess.check_output(["cta-admin --json fr ls -S"], shell=True)
+cta_admin_output = subprocess.check_output(["cta-admin", "--json", "fr", "ls", "-S"], shell=True)
 
 cta_admin_output_json = json.loads(cta_admin_output)
 
