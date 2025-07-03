cd4a8408c388dc0ceb8c1d94a798d4923044f336
danijelaconc <dmc24@stmarys-ca.edu>
2025-07-03 14:48:58 -0500

    implementing common.py function into all codes

--- docker/cloudprober-cta-admin/cta_admin_failedrequest_ls.py
+++ docker/cloudprober-cta-admin/cta_admin_failedrequest_ls.py
@@ -2,23 +2,7 @@
 
 import json
 import subprocess
-
-
-def produce_prom_metric(metric_name, metric_value, list_input, labels):
-    # loop over labels to get key,value pairs
-    # print(len(labels))
-    # print(metric_name)
-    label_list = []
-    for label in labels:
-        # print(label)
-        label_list += [label + '="' + list_input[label] + '"']
-        # print(label_list)
-
-    print(metric_name, end='')
-    print('{', ', '.join(label_list), end='')  # ,sep = ", "
-    print('}', end='')
-    print(f' {metric_value}')
-
+from common import produce_prom_metric
 
 cta_admin_output = subprocess.check_output(["cta-admin", "--json", "fr", "ls", "-S"], shell=True)
 
