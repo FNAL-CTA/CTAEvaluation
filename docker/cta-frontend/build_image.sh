5fa11df06574fed29487f5084a0028f512743722
Eric Vaandering <ewv@fnal.gov>
2025-10-13 11:01:02 -0500

    Ability to build 5.11.12

--- docker/cta-frontend/build_image.sh
+++ docker/cta-frontend/build_image.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-podman build --net host . -t imageregistry.fnal.gov/cta/cta-frontend:5.11.9.fnal1
-podman push imageregistry.fnal.gov/cta/cta-frontend:5.11.9.fnal1
+podman build --net host . -t imageregistry.fnal.gov/cta/cta-frontend:5.11.12.fnal1
+podman push imageregistry.fnal.gov/cta/cta-frontend:5.11.12.fnal1
 
