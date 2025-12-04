32fa326ca3b0a73cb58818552443d6ccb2ffeeb7
Eric Vaandering <ewv@fnal.gov>
2025-12-04 14:37:32 -0600

    Push forward to 5.11.13

--- docker/cta-fluentd/build_images_Fluentd.sh
+++ docker/cta-fluentd/build_images_Fluentd.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-podman build --net host . -t imageregistry.fnal.gov/cta/cta-fluentd:5.11.9.fnal1
-podman push imageregistry.fnal.gov/cta/cta-fluentd:5.11.9.fnal1
+podman build --net host . -t imageregistry.fnal.gov/cta/cta-fluentd:5.11.13.fnal1
+podman push imageregistry.fnal.gov/cta/cta-fluentd:5.11.13.fnal1
 
 #podman build --net host . -f Containerfile.logs -t imageregistry.fnal.gov/cta/cta-logs-fluentd:0.0.1
 #podman push imageregistry.fnal.gov/cta/cta-logs-fluentd:0.0.1
