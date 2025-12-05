ae1b26a53300412396d1af233b9284c667067a9d
Eric Vaandering <ewv@fnal.gov>
2025-12-05 15:01:35 -0500

    Tracing (#65)
    * Ability to build 5.11.12
    
    * Push forward to 5.11.13
    
    * More to 5.11.13
    
    * Even more

--- docker/cloudprober-cta-admin/build_image.sh
+++ docker/cloudprober-cta-admin/build_image.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-podman build --net host . -t imageregistry.fnal.gov/cta/cloudprober-cta-admin:5.11.9.fnal2
-podman push imageregistry.fnal.gov/cta/cloudprober-cta-admin:5.11.9.fnal2
+podman build --net host . -t imageregistry.fnal.gov/cta/cloudprober-cta-admin:5.11.13.fnal1
+podman push imageregistry.fnal.gov/cta/cloudprober-cta-admin:5.11.13.fnal1
